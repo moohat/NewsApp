99dfadb93d8c01ad01594f76cae13f8054a92d2b
taufik m <refizcorp@gmail.com>
2020-06-25 17:44:20 +0700

    styling Tab Content using native base

---
+++ src/config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'id';
+export const category = 'general';
+export const _api_key = 'e878e4729bb446fa8756b030f52ab828';
--- src/screens/TabGeneral/index.js
+++ src/screens/TabGeneral/index.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
-export default class TabGeneral extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Tab General Page </Text>
-            </View>
-        )
-    }
+import React, {Component} from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+} from 'native-base';
+export default class ListThumbnailExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Content>
+          <List>
+            <ListItem thumbnail>
+              <Left>
+                <Thumbnail
+                  square
+                  source={{
+                    uri:
+                      'https://img1.rapidleaks.com/2018/09/Most-Beautiful-Girls-on-the-Earth-1280x720.jpg',
+                  }}
+                />
+              </Left>
+              <Body>
+                <Text>News Title</Text>
+                <Text note numberOfLines={2}>
+                  Its time to build a difference . .
+                </Text>
+              </Body>
+              <Right>
+                <Button transparent>
+                  <Text>View</Text>
+                </Button>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
 }
